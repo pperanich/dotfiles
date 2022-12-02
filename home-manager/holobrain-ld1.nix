6179a3190ae5690817e4c9d1a16dcb42edca5dc7
Preston Peranich <pperanich@gmail.com>
2022-12-02 10:54:09 -0500

    Updating holobrain-ld1 nix.

--- home-manager/holobrain-ld1.nix
+++ home-manager/holobrain-ld1.nix
@@ -4,6 +4,7 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
   # You can import other home-manager modules here
   imports = [
+    ./global
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
 

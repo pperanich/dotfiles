f0ee2cc9c7fc6774e76f509a9ad64363b34246ab
Preston Peranich <pperanich@gmail.com>
2022-12-20 19:25:29 -0600

    Emacs now working on mac

--- home-manager/features/emacs.nix
+++ home-manager/features/emacs.nix
@@ -2,50 +2,22 @@
 let
   inherit (config.home) homeDirectory;
   inherit (config.lib.file) mkOutOfStoreSymlink;
-  inherit (pkgs) fetchFromGitHub;
 
-  # emacs = inputs.emacs-overlay.packages.${pkgs.system}.emacsGit.overrideAttrs (attrs: {
-  my-emacs = pkgs.emacs.overrideAttrs (attrs: {
-    # patches = (attrs.patches or [ ]) ++ [
-    #   ./patches/poll.patch
-    # ];
-    # configureFlags = attrs.configureFlags ++ ["--with-poll"];
-    macportVersion = "emacs-28.2-mac-9.1";
-  });
-
+  emacs = if pkgs.stdenv.hostPlatform.isDarwin then
+    pkgs.emacsMacport.override { withMacport = true; withSQLite3 = true; withWebP = true; withImageMagick = true; }
+    else
+    inputs.emacs-overlay.packages.${pkgs.system}.emacsGit.override { withImageMagick = true; };
 in
 {
   programs.emacs = {
-    enable = true;
-    package = my-emacs.override { withMacport = true; withSQLite3 = true; withWebP = true; withImageMagick = true; };
-    # package = emacs.override { withMacport = true; };
+  enable = true;
+  package = emacs;
   };
 
-  xdg.configFile."emacs".source = fetchFromGitHub 
-  {
-    owner = "plexus"; repo = "chemacs2";
-    rev = "fb6301d1563c6cf88c8aac51ff8bcd4e06276139";
-    sha256 = "sha256-cYIC5P0t7YJxfAF8g7Q2Stfz8+wrqXLonrfsEhotVY0=";
-  };
+  xdg.configFile."emacs".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/emacs-chemacs/";
   xdg.configFile."chemacs".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/chemacs/";
-
-  # xdg.configFile."emacs-spacemacs".source = fetchFromGitHub 
-  # {
-  #   owner = "syl20bnr";
-  #   repo = "spacemacs";
-  #   rev = "b74da79dbbd8573ab4f43721c74014d6f54d8bd0";
-  #   sha256 = "sha256-ysydhBD1FqA22E7wejcfaElTL/BjC6Obi9LA87knmVI=";
-  # };
-  xdg.configFile."spacemacs".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/spacemacs/";
   xdg.configFile."emacs-spacemacs".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/emacs-spacemacs/";
-
-  # xdg.configFile."emacs-doom".source = fetchFromGitHub 
-  # {
-  #   owner = "doomemacs";
-  #   repo = "doomemacs";
-  #   rev = "d5ccac5d71c819035fa251f01d023b3f94b4fba4";
-  #   sha256 = "sha256-7AzL08qo5WLeJo10lnF2b7g6FdWnExVYS8yipNyAMMM=";
-  # };
-  xdg.configFile."doom".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/doom/";
+  xdg.configFile."spacemacs".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/spacemacs/";
   xdg.configFile."emacs-doom".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/emacs-doom/";
+  xdg.configFile."doom".source = mkOutOfStoreSymlink "${homeDirectory}/dotfiles/config/doom/";
 }

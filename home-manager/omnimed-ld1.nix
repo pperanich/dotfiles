13b20c9090670c721260c880d0676320f46fea5f
Preston Peranich <pperanich@gmail.com>
2022-12-24 13:55:39 -0500

    Updating omnimed-ld1.nix

--- home-manager/omnimed-ld1.nix
+++ home-manager/omnimed-ld1.nix
@@ -4,6 +4,7 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
   # You can import other home-manager modules here
   imports = [
+    ./global
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
 
@@ -38,10 +39,9 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "omni";
+    homeDirectory = "/home/omni";
   };
 
   # Add stuff for your user as you see fit:
@@ -56,5 +56,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "22.05";
+  home.stateVersion = "22.11";
 }

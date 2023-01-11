904ed7e78f179c2693318025bc11d58541894eb2
Preston Peranich <pperanich@gmail.com>
2023-01-11 13:41:11 -0500

    Updating flake. Specifying package rev for nvim to fix build issue.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,12 @@
 
     hardware.url = "github:nixos/nixos-hardware";
 
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      # Pin to a nixpkgs revision that doesn't have NixOS/nixpkgs#208103 yet
+      inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+    };
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 

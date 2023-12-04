a46f4a7cf49cffe0e72af7db5015840c91d6af35
Preston Peranich <pperanich@gmail.com>
2023-12-04 17:57:08 -0500

    Adding twitter emoji font back to install. Build issue resolved.

--- home-manager/features/fonts.nix
+++ home-manager/features/fonts.nix
@@ -4,7 +4,7 @@
     (pkgs.nerdfonts.override { fonts = [ "SourceCodePro" "Iosevka" "IBMPlexMono" "Overpass" "FiraMono" "FiraCode" ]; })
 
     # Add below once the following is closed: https://github.com/NixOS/nixpkgs/issues/270222
-    # pkgs.twitter-color-emoji
+    pkgs.twitter-color-emoji
   ];
 
   # required to autoload fonts from packages installed via Home Manager

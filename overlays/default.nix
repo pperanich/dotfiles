517fd2c3482b18e146be10e7c06cf6ed2c31939c
Preston Peranich <pperanich@gmail.com>
2024-02-20 11:29:53 -0500

    Patching tms to expand environment variables.

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,13 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
+    tmux-sessionizer = prev.tmux-sessionizer.overrideAttrs (old: {
+      patches =
+        (old.patches or [ ])
+        ++ [
+          ../home-manager/features/patches/0001-Expand-env-vars-and-tilde-for-search_dirs.patch
+        ];
+    });
     heygpt = prev.heygpt.overrideAttrs (oldAttrs: rec {
       buildInputs = oldAttrs.buildInputs ++ final.lib.optionals final.stdenv.hostPlatform.isDarwin [ final.darwin.apple_sdk.frameworks.SystemConfiguration ];
     });

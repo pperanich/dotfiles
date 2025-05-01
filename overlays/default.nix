9926f7b56fdae1f7ba4071bcc53b61d9cf5ae12c
Preston Peranich <pperanich@gmail.com>
2025-05-01 11:11:52 -0400

    feat: cleanup of overlays.

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
   # This one brings our my packages from the 'pkgs' directory
   emacs-overlay = inputs.emacs-overlay.overlays.default;
   neovim-overlay = inputs.neovim-nightly-overlay.overlays.default;
-  # ghostty-overlay = inputs.ghostty.overlays.default;
 
   nixgl = inputs.nixgl.overlay;
   rust-overlay = inputs.rust-overlay.overlays.default;
@@ -16,6 +15,14 @@
       mkdir -p $out/etc/ssl/certs
       cp ${./JHUAPL-MS-Root-CA-05-21-2038-B64-text.crt} $out/etc/ssl/certs/apl-ca.crt
     '';
+    cacert-apl = prev.cacert.override {
+      extraCertificateFiles = [./JHUAPL-MS-Root-CA-05-21-2038-B64-text.crt];
+    };
+    curl-openssl_1_1 = prev.curl.override {openssl = prev.openssl_1_1;};
+    git-openssl_1_1 = prev.git.override {
+      openssl = prev.openssl_1_1;
+      curl = final.curl-openssl_1_1;
+    };
 
     atuin = prev.atuin.overrideAttrs (old: {
       # as cursed as doing mitigations=off in the kernel command line
@@ -46,82 +53,17 @@
         buildInputs = (prev.buildInputs or []) // [prev.openssl_1_1];
       };
 
-    # buildGoModule = prev.buildGoModule // {
-    #   preBuild = builtins.trace "Running prebuild" ''
-    #   echo "Hello world"
-    #   export GODEBUG=x509negativeserial=1
-    #   exit 234
-    #   '';
-    #   env = {
-    #     NIX_SSL_CERT_FILE = final.aplCertificate;
-    #     SSL_CERT_FILE = final.aplCertificate;
-    #     GIT_SSL_CAINFO= final.aplCertificate;
-    #     GODEBUG= "x509negativeserial=1";
-    #     GOEXPERIMENT="x509negativeserial";
-    #     };
-    #     GODEBUG= "x509negativeserial=1";
-    # };
-  
     buildGoModule = prev.buildGoModule.override {
-      cacert = prev.cacert.override {
-        extraCertificateFiles = [./JHUAPL-MS-Root-CA-05-21-2038-B64-text.crt];
-      };
+      cacert = final.cacert-apl;
     };
 
-    # sops-install-secrets = prev.sops-install-secrets.overrideAttrs (
-    #   oldAttrs: {
-    #     env = {
-    #       NIX_SSL_CERT_FILE = final.aplCertificate;
-    #       SSL_CERT_FILE = final.aplCertificate;
-    #       GIT_SSL_CAINFO= final.aplCertificate;
-    #       GODEBUG= "x509negativeserial=1";
-    #       GOEXPERIMENT="x509negativeserial";
-    #       };
-    #       GODEBUG= "x509negativeserial=1";
-    # });
-
-    # sops-install-secrets = prev.sops-install-secrets.overrideAttrs (oldAttrs: {
-    #   buildPhase = throw ''
-    #     export GODEBUG=x509negativeserial=1
-    #     ${oldAttrs.buildPhase}
-    #   '';
-    #   env = {
-    #     NIX_SSL_CERT_FILE = final.aplCertificate;
-    #     SSL_CERT_FILE = final.aplCertificate;
-    #     GIT_SSL_CAINFO= final.aplCertificate;
-    #     GODEBUG= "x509negativeserial=1";
-    #     GOEXPERIMENT="x509negativeserial";
-    #     };
-    #     GODEBUG= "x509negativeserial=1";
-    # });
-    # buildGoModule = prev.buildGoModule // {
-    #   buildGoModule = args:
-    #   prev.buildGoModule // {
-    #     env = {
-    #       NIX_SSL_CERT_FILE = final.aplCertificate;
-    #       SSL_CERT_FILE = final.aplCertificate;
-    #       GIT_SSL_CAINFO= final.aplCertificate;
-    #       GODEBUG= "x509negativeserial=1";
-    #       GOEXPERIMENT="x509negativeserial";
-    #     };
-    #   }
-    # (args // {});
-    # };
-
-    curl-openssl_1_1 = prev.curl.override {openssl = prev.openssl_1_1;};
-    git-openssl_1_1 = prev.git.override {
-      openssl = prev.openssl_1_1;
-      curl = final.curl-openssl_1_1;
-    };
     rustPlatform =
       prev.rustPlatform
       // {
         buildRustPackage = args:
           prev.rustPlatform.buildRustPackage.override {
             fetchCargoVendor = prev.rustPlatform.fetchCargoVendor.override {
-              cacert = prev.cacert.override {
-                extraCertificateFiles = [./JHUAPL-MS-Root-CA-05-21-2038-B64-text.crt];
-              };
+              cacert = final.cacert-apl;
             };
           }
           (args // {});

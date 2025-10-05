1d31d0c70d9580a3c7189b0b77a32e6c8373cb7e
Preston Peranich <pperanich@gmail.com>
2025-10-05 15:41:42 -0400

    feat: remove pkgs.nix from dev shell.

--- modules/flake-parts/shell.nix
+++ modules/flake-parts/shell.nix
@@ -10,16 +10,7 @@
       devShells.default = pkgs.mkShellNoCC {
         name = "dotfiles-shell";
 
-        NIX_CONFIG = "extra-experimental-features = nix-command flakes";
         packages = [
-          # Nix tools
-          pkgs.nix
-          pkgs.home-manager
-          pkgs.alejandra
-
-          # System tools
-          pkgs.git
-
           # Secret management
           pkgs.sops
           pkgs.ssh-to-age
@@ -27,20 +18,11 @@
           pkgs.age
           pkgs.age-plugin-se
 
-          pkgs.nixVersions.latest
-
           inputs'.clan-core.packages.clan-cli
 
           # treefmt with config defined in fmt.nix
           config.treefmt.build.wrapper
         ];
-        shellHook = ''
-          echo "Welcome to the dotfiles development shell!"
-          echo "Available flake outputs:"
-          echo " - nixosConfigurations"
-          echo " - darwinConfigurations"
-          echo " - homeConfigurations"
-        '';
       };
     };
 }

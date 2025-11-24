f276f2ecdf404add3980b1c370f3b58cfb90f30f
Preston Peranich <pperanich@gmail.com>
2025-11-24 18:20:25 -0500

    feat(router): add Unbound DNS with local zone and DoT upstream

---
+++ modules/router/dns.nix
@@ -0,0 +1,91 @@
+_: {
+  flake.modules.nixos.routerDns =
+    {
+      config,
+      lib,
+      ...
+    }:
+    let
+      cfg = config.features.router;
+      dnsCfg = cfg.dns;
+      internal = cfg._internal;
+      lanSubnet = internal.lanSubnet;
+      lanCidr = internal.lanCidr;
+      routerIp = internal.routerIp;
+      ulaPrefix = cfg.ipv6.ulaPrefix;
+      inherit (cfg) machines services;
+      enabled = cfg.enable && dnsCfg.enable;
+    in
+    {
+      options.features.router.dns = {
+        upstreamServers = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [
+            "1.1.1.1@853#cloudflare-dns.com"
+            "1.0.0.1@853#cloudflare-dns.com"
+          ];
+          description = "Upstream DNS servers (with DoT)";
+        };
+        localZone = lib.mkOption {
+          type = lib.types.str;
+          default = "lan.";
+          description = "Local DNS zone name";
+        };
+      };
+
+      config = lib.mkIf enabled {
+        services.unbound = {
+          enable = true;
+          settings = {
+            remote-control.control-enable = true;
+            server = {
+              interface = [
+                "127.0.0.1"
+                "::1"
+                routerIp
+              ] ++ lib.optional cfg.ipv6.enable "${ulaPrefix}::1";
+
+              access-control = [
+                "127.0.0.0/8 allow"
+                "::1 allow"
+                "${lanCidr} allow"
+              ] ++ lib.optional cfg.ipv6.enable "${ulaPrefix}::/64 allow"
+              ++ [
+                "0.0.0.0/0 refuse"
+                "::0/0 refuse"
+              ];
+
+              cache-min-ttl = 0;
+              cache-max-ttl = 86400;
+              do-tcp = true;
+              do-udp = true;
+              prefetch = true;
+              num-threads = 2;
+              so-reuseport = true;
+
+              # Local zone for LAN
+              local-zone = "\"${dnsCfg.localZone}\" static";
+              local-data =
+                [
+                  "\"${cfg.hostname}.${dnsCfg.localZone} IN A ${routerIp}\""
+                ]
+                ++ lib.optional cfg.ipv6.enable
+                  "\"${cfg.hostname}.${dnsCfg.localZone} IN AAAA ${ulaPrefix}::1\""
+                ++ map (
+                  machine:
+                  "\"${machine.name}.${dnsCfg.localZone} IN A ${lanSubnet}.${toString machine.ip}\""
+                ) machines
+                ++ map (service: "\"${service.name} IN A ${service.target}\"") services;
+            };
+            forward-zone = [
+              {
+                name = ".";
+                forward-addr = dnsCfg.upstreamServers;
+                forward-tls-upstream = true;
+              }
+            ];
+          };
+        };
+      };
+    };
+}

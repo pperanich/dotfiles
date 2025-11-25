f3dc7d05b661dfdcea668e31200156592263ff74
Preston Peranich <pperanich@gmail.com>
2025-11-24 22:48:23 -0500

    feat: minor aerospace updates.

--- home/.config/sketchybar/lua/bar.lua
+++ home/.config/sketchybar/lua/bar.lua
@@ -2,13 +2,13 @@ local colors = require("lua.colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-    height = 36,
-    color = colors.transparent,
-    display = "all",
-    topmost = "window",
-    padding_right = 10,
-    padding_left = 10,
-    margin = 3,
-    y_offset = 3,
-    corner_radius = 12,
+	height = 36,
+	color = colors.transparent,
+	display = "all",
+	topmost = "window",
+	padding_right = 10,
+	padding_left = 10,
+	margin = 3,
+	y_offset = 3,
+	corner_radius = 12,
 })

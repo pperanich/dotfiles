393e239440b3b7fecd3bc528f12fa52070143e31
Preston Peranich <pperanich@gmail.com>
2026-01-31 11:10:50 -0500

    feat: working on router configuration.

--- machines/pp-router1/configuration.nix
+++ machines/pp-router1/configuration.nix
@@ -7,6 +7,7 @@
 }:
 {
   imports = [
+    ./disko.nix
     inputs.nixos-facter-modules.nixosModules.facter
   ]
   ++ (with modules.nixos; [
---
+++ machines/pp-router1/disko.nix
@@ -0,0 +1,52 @@
+# ---
+# schema = "single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-id/PLACEHOLDER_DISK_ID"
+# ---
+# PLACEHOLDER: Update mainDisk above after running:
+#   ls -la /dev/disk/by-id/
+# on the actual hardware to find the correct disk identifier.
+{
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    enable = true;
+  };
+  disko.devices = {
+    disk = {
+      main = {
+        name = "main-pp-router1";
+        device = "/dev/disk/by-id/PLACEHOLDER_DISK_ID";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

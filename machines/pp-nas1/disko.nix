22c6ecb201af6921ad40781bb5ee1648f98adb0c
Preston Peranich <pperanich@gmail.com>
2026-02-12 19:28:01 -0500

    feat: working on nas disko

--- machines/pp-nas1/disko.nix
+++ machines/pp-nas1/disko.nix
@@ -49,3 +49,177 @@
     };
   };
 }
+
+### I want to move to the following once we have disk info from the machine.
+#
+## disko.nix
+# { ... }:
+#
+# {
+#   disko.devices = {
+#     disk = {
+#       nvme0 = {
+#         type = "disk";
+#         device = "/dev/nvme0n1";
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             ESP = {
+#               size = "1G";
+#               type = "EF00";
+#               content = {
+#                 type = "filesystem";
+#                 format = "vfat";
+#                 mountpoint = "/boot";
+#                 mountOptions = [ "umask=0077" ];
+#               };
+#             };
+#
+#             zfs = {
+#               size = "100%";
+#               content = {
+#                 type = "zfs";
+#                 pool = "rpool";
+#               };
+#             };
+#           };
+#         };
+#       };
+#
+#       nvme1 = {
+#         type = "disk";
+#         device = "/dev/nvme1n1";
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             ESP = {
+#               size = "1G";
+#               type = "EF00";
+#               content = {
+#                 type = "filesystem";
+#                 format = "vfat";
+#                 mountpoint = "/boot2";
+#                 mountOptions = [ "umask=0077" ];
+#               };
+#             };
+#
+#             zfs = {
+#               size = "100%";
+#               content = {
+#                 type = "zfs";
+#                 pool = "rpool";
+#               };
+#             };
+#           };
+#         };
+#       };
+#     };
+#
+#     zpool = {
+#       rpool = {
+#         type = "zpool";
+#         mode = "mirror";
+#
+#         # These apply to datasets by default (you can override per-dataset).
+#         rootFsOptions = {
+#           compression = "zstd";
+#           atime = "off";
+#           xattr = "sa";
+#           acltype = "posixacl";
+#           normalization = "formD";
+#           "com.sun:auto-snapshot" = "false";
+#         };
+#
+#         datasets = {
+#           # Root dataset
+#           root = {
+#             type = "zfs_fs";
+#             mountpoint = "/";
+#             options = {
+#               canmount = "noauto";
+#             };
+#           };
+#
+#           # This is what NixOS expects for ZFS root layouts.
+#           "root/nixos" = {
+#             type = "zfs_fs";
+#             mountpoint = "/";
+#             options = {
+#               canmount = "noauto";
+#             };
+#           };
+#
+#           nix = {
+#             type = "zfs_fs";
+#             mountpoint = "/nix";
+#             options = { "com.sun:auto-snapshot" = "false"; };
+#           };
+#
+#           var = {
+#             type = "zfs_fs";
+#             mountpoint = "/var";
+#           };
+#
+#           home = {
+#             type = "zfs_fs";
+#             mountpoint = "/home";
+#           };
+#
+#           tank = {
+#             type = "zfs_fs";
+#             mountpoint = "/tank";
+#             options = { "com.sun:auto-snapshot" = "true"; };
+#           };
+#
+#           "tank/appdata" = {
+#             type = "zfs_fs";
+#             mountpoint = "/tank/appdata";
+#             options = { "com.sun:auto-snapshot" = "true"; };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
+#
+#
+# inside disko.devices.zpool.rpool.datasets = { ... };
+#
+# "root/nixos" = {
+#   type = "zfs_fs";
+#   mountpoint = "/";
+#   options = {
+#     canmount = "noauto";
+#     # Optional: limit root dataset itself (often small)
+#     quota = "50G";
+#   };
+# };
+#
+# nix = {
+#   type = "zfs_fs";
+#   mountpoint = "/nix";
+#   options = {
+#     # This is the one that tends to grow on NixOS
+#     quota = "250G";  # example cap
+#     "com.sun:auto-snapshot" = "false";
+#   };
+# };
+#
+# var = {
+#   type = "zfs_fs";
+#   mountpoint = "/var";
+#   options = {
+#     quota = "100G"; # example cap
+#   };
+# };
+#
+# tank = {
+#   type = "zfs_fs";
+#   mountpoint = "/tank";
+#   options = {
+#     # Protect data space from being squeezed by OS datasets
+#     reservation = "1500G";  # example "guaranteed" space for data
+#     "com.sun:auto-snapshot" = "true";
+#   };
+# };
+#
